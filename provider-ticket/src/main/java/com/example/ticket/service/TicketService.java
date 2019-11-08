49c66ee4b8177876ada28d03737e2de1c8b1731b
chunhongZheng <824394795@qq.com>
2019-11-08 15:13:34 +0800

    empty project .idea2

--- provider-ticket/src/main/java/com/example/ticket/service/TicketService.java
+++ provider-ticket/src/main/java/com/example/ticket/service/TicketService.java
@@ -3,4 +3,5 @@ package com.example.ticket.service;
 public interface TicketService {
     public String getTicket();
     public String sayHello();
+    public String watchTV();
 }
--- provider-ticket/src/main/java/com/example/ticket/service/impl/TicketServiceImpl.java
+++ provider-ticket/src/main/java/com/example/ticket/service/impl/TicketServiceImpl.java
@@ -11,6 +11,11 @@ public class TicketServiceImpl implements TicketService {
     }
 
     @Override
+    public String watchTV() {
+        return "watchTV";
+    }
+
+    @Override
     public String getTicket() {
         System.out.println("8003");
         return "中国梦8003";
